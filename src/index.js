0b2e5d7b09d28c58097005142503e29ef55cf900
huyen vu <huyen.vu@nextgencode.io>
2019-02-12 21:13:13 -0600

    express server up and running

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/divisions', function(req,res){
+    res.json({data: [{id: 0, name: "division 1"}]});
+});
+
+app.get('*', function(req,res){
+    res.json({success: false, message: 'wrong route, buddy!'});
+})
+
+app.listen(8080, function(){
+    console.log('listening on port 8080');
+})
+
